e8b6c4d838530b7989ab885e10965a1cc83c7558
beshoux <beshoux@DESKTOP-DHDQV9L.localdomain>
2022-10-31 19:42:13 +0100

    Ajout de la gravité.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
+const gravity = 0.2
+
 // Taille de la fenêtre.
 canvas.width = 1024
 canvas.height = 576
@@ -13,18 +15,26 @@ class Sprite {
   constructor({ pos, vitesse }) {
     this.pos = pos
     this.vitesse = vitesse
+    this.height = 150
   }
 
   // Methode pour afficher les sprites.
   draw() {
     c.fillStyle = 'red'
-    c.fillRect(this.pos.x, this.pos.y, 50, 150)
+    c.fillRect(this.pos.x, this.pos.y, 50, this.height)
   }
 
   // Methode pour mettre a jour, les pos des personnages.
   update_pos() {
     this.draw()
     this.pos.y += this.vitesse.y
+
+    // Tant que le perso est en l'air, on mettra 0 comme vitesse y, pour le faire descendre.
+    if (this.pos.y + this.height + this.vitesse.y >= canvas.height) {
+      this.vitesse.y = 0
+    } else {
+      this.vitesse.y += gravity
+    }
   }
 }
 
