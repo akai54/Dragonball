4485947ff90039b95d114e63c8510528a0910904
beshoux <beshoux@DESKTOP-DHDQV9L.localdomain>
2022-10-31 17:14:59 +0100

    Premiers proprietes etablis.

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
+
+// Taille de la fenetre.
+canvas.width = 1024
+canvas.height = 576
+
+// Changer la couleur du fond de la fenetre.
+c.fillRect(0, 0, canvas.width, canvas.height)
