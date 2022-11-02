5f89c6c3e2aaa763e7fb64fb56ef4a140f2bf046
beshoux <beshoux@outlook.fr>
2022-11-02 21:35:47 +0100

    Mettre les fonctions d'utilité dans un autre fichier.

--- js/script.js
+++ js/script.js
@@ -84,42 +84,6 @@ const touches = {
   },
 }
 
-function collision_joueurs({ j1, j2 }) {
-  return (
-    j1.attackBox.pos.x + j1.attackBox.width >= j2.attackBox.pos.x &&
-    j1.attackBox.pos.x <= j2.pos.x + j2.width &&
-    j1.attackBox.pos.y + j1.attackBox.height >= j2.pos.y &&
-    j1.attackBox.pos.y <= j2.pos.y + j2.height &&
-    j1.isAttacking
-  )
-}
-
-function fin_jeu({ joueur, joueur2, timerId }) {
-  clearTimeout(timerId)
-  document.querySelector('#timerRes').style.display = 'flex'
-  if (joueur.vie === joueur2.vie) {
-    document.querySelector('#timerRes').innerHTML = 'Égalité'
-  } else if (joueur.vie > joueur2.vie) {
-    document.querySelector('#timerRes').innerHTML = 'Joueur1 a gagné'
-  } else if (joueur.vie < joueur2.vie) {
-    document.querySelector('#timerRes').innerHTML = 'Joueur2 a gagné'
-  }
-}
-
-let timer = 10
-let timerId
-function dec_Timer() {
-  if (timer > 0) {
-    timerId = setTimeout(dec_Timer, 1000)
-    timer--
-    document.querySelector('#timer').innerHTML = timer
-  }
-
-  if (timer === 0) {
-    fin_jeu({ joueur, joueur2, timerId })
-  }
-}
-
 dec_Timer()
 
 // La fonction qui va etre appeler en boucle.
