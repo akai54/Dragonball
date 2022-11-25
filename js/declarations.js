35419e419a0d5f7d6a5af1fe5d8f459ddfa2c752
beshoux <beshoux@outlook.fr>
2022-11-25 20:25:39 +0100

    Toutes les declarations dans le meme fichier.

---
+++ js/declarations.js
@@ -0,0 +1,48 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+// Taille de la fenêtre.
+canvas.width = 1024
+canvas.height = 576
+
+// Changer la couleur du fond de la fenêtre.
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+//Musique Jeu.
+const dbz_music = document.createElement('audio')
+dbz_music.src = './ressources/sounds/musics/DBZ_theme.ogg'
+const dbz_menu = document.createElement('audio')
+dbz_menu.src = './ressources/sounds/musics/DBZ_overTheGalaxy.ogg'
+const ko = document.createElement('audio')
+ko.src = './ressources/sounds/effects/DBZ_KO.ogg'
+const fight_banner = document.createElement('audio')
+fight_banner.src = './ressources/sounds/effects/DBZ_FIGHT.ogg'
+
+//Sprites GOKU.
+const gokuBlock = document.createElement('audio')
+gokuBlock.src = './ressources/sounds/voices/goku/gokuBlock.ogg'
+const goku1 = document.createElement('audio')
+goku1.src = './ressources/sounds/voices/goku/gokuHandAttack.ogg'
+const goku2 = document.createElement('audio')
+goku2.src = './ressources/sounds/voices/goku/gokuJumpKick.ogg'
+const goku3 = document.createElement('audio')
+goku3.src = './ressources/sounds/voices/goku/gokuRushAttack.ogg'
+const gokuHit = document.createElement('audio')
+gokuHit.src = './ressources/sounds/voices/goku/gokuTouchedPropels.ogg'
+
+//Sprites VEGETA.
+const vegetaHit = document.createElement('audio')
+vegetaHit.src = './ressources/sounds/voices/vegeta/6.wav'
+const vegeta1 = document.createElement('audio')
+vegeta1.src = './ressources/sounds/voices/vegeta/34.wav'
+const vegeta2 = document.createElement('audio')
+vegeta2.src = './ressources/sounds/voices/vegeta/35.wav'
+const vegeta3 = document.createElement('audio')
+vegeta3.src = './ressources/sounds/voices/vegeta/36.wav'
+const vegetaBlock = document.createElement('audio')
+vegetaBlock.src = './ressources/sounds/voices/vegeta/19.wav'
+
+const gravity = 0.8
+let gameStarted = false
+let startTimer = false
+let menu = true
--- js/script.js
+++ js/script.js
@@ -1,42 +1,6 @@
-const canvas = document.querySelector('canvas')
-
-const c = canvas.getContext('2d')
-
-const dbz_music = document.createElement('audio')
-dbz_music.src = './ressources/sounds/musics/DBZ_theme.ogg'
-const dbz_menu = document.createElement('audio')
-dbz_menu.src = './ressources/sounds/musics/DBZ_overTheGalaxy.ogg'
-const gokuBlock = document.createElement('audio')
-gokuBlock.src = './ressources/sounds/voices/goku/gokuBlock.ogg'
-const goku1 = document.createElement('audio')
-goku1.src = './ressources/sounds/voices/goku/gokuHandAttack.ogg'
-const goku2 = document.createElement('audio')
-goku2.src = './ressources/sounds/voices/goku/gokuJumpKick.ogg'
-const goku3 = document.createElement('audio')
-goku3.src = './ressources/sounds/voices/goku/gokuRushAttack.ogg'
-const gokuHit = document.createElement('audio')
-gokuHit.src = './ressources/sounds/voices/goku/gokuTouchedPropels.ogg'
-const ko = document.createElement('audio')
-ko.src = './ressources/sounds/effects/DBZ_KO.ogg'
-const fight_banner = document.createElement('audio')
-fight_banner.src = './ressources/sounds/effects/DBZ_FIGHT.ogg'
-const vegetaHit = document.createElement('audio')
-vegetaHit.src = './ressources/sounds/voices/vegeta/6.wav'
-const vegeta1 = document.createElement('audio')
-vegeta1.src = './ressources/sounds/voices/vegeta/34.wav'
-const vegeta2 = document.createElement('audio')
-vegeta2.src = './ressources/sounds/voices/vegeta/35.wav'
-const vegeta3 = document.createElement('audio')
-vegeta3.src = './ressources/sounds/voices/vegeta/36.wav'
-const vegetaBlock = document.createElement('audio')
-vegetaBlock.src = './ressources/sounds/voices/vegeta/19.wav'
 const btnJouer = document.querySelector('#jouer')
 
-let gameStarted = false
-let startTimer = false
-let menu = true
-
-const gravity = 0.8
+//Image de fond.
 const bg = new Sprite({
   pos: {
     x: 0,
@@ -44,6 +8,8 @@ const bg = new Sprite({
   },
   imgSrc: './ressources/bg/test.png',
 })
+
+//Image de fond du menu.
 const menuImg = new Sprite({
   pos: {
     x: 0,
@@ -52,6 +18,7 @@ const menuImg = new Sprite({
   imgSrc: './ressources/bg/menu.png',
 })
 
+//Image READY.
 const ready = new Sprite({
   pos: {
     x: 300,
@@ -60,6 +27,7 @@ const ready = new Sprite({
   imgSrc: './ressources/bg/ready.png',
 })
 
+//Image FIGHT.
 const fighting = new Sprite({
   pos: {
     x: 300,
@@ -67,13 +35,6 @@ const fighting = new Sprite({
   },
   imgSrc: './ressources/bg/fight.png',
 })
-
-// Taille de la fenêtre.
-canvas.width = 1024
-canvas.height = 576
-
-// Changer la couleur du fond de la fenêtre.
-c.fillRect(0, 0, canvas.width, canvas.height)
 
 // Le joueur principale.
 const joueur1 = new Joueur({
